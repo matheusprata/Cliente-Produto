2e5a9ec43e93601e58305115fa84e7c097b5e417
matheusprata <103780607+matheusprata@users.noreply.github.com>
2023-03-07 09:40:02 -0300

    inicio do projeto

---
+++ src/main/java/br/com/bonfimvariedades/ClienteFiado/ClienteFiadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bonfimvariedades.ClienteFiado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteFiadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteFiadoApplication.class, args);
+	}
+
+}
