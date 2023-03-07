196736ae8aa76674f8a10401da49bc1748b458cf
matheusprata <103780607+matheusprata@users.noreply.github.com>
2023-03-07 10:34:38 -0300

    implementa application e home

--- src/main/java/br/com/bonfimvariedades/ClienteFiado/ClienteFiadoApplication.java
+++ src/main/java/br/com/bonfimvariedades/ClienteFiado/ClienteFiadoApplication.java
@@ -2,10 +2,19 @@ package br.com.bonfimvariedades.ClienteFiado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClienteFiadoApplication {
 
+	@GetMapping
+	public String getHometeste() {
+		return "API CLIENTE-FIADO";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteFiadoApplication.class, args);
 	}
