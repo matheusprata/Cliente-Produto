0594db7da88d0e71fcdf72ccf6498980decea67e
matheusprata <matheusprata.ti@gmail.com>
2023-03-13 22:30:32 -0300

    implementa request e response

--- src/main/java/br/com/bonfimvariedades/clientefiado/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/bonfimvariedades/clientefiado/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,12 @@
 package br.com.bonfimvariedades.clientefiado.cliente.application.api;
 
-public class ClienteRequest {
+import java.util.UUID;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class ClienteRequest {
+	private UUID idCliente;
 }
