6105fc24f43af3352254090c5f5266c7607ab336
matheusprata <matheusprata.ti@gmail.com>
2023-03-13 23:14:32 -0300

    implementa clienteApplicationService e novo contrutor cliente, teste ok

--- src/main/java/br/com/bonfimvariedades/clientefiado/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/bonfimvariedades/clientefiado/cliente/application/service/ClienteApplicationService.java
@@ -19,9 +19,9 @@ public class ClienteApplicationService implements ClienteService {
 	@Override
 	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaCliente");
-		Cliente cliente = clienteRepository.salva(new Cliente(ClienteRequest));
+		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService - criaCliente");
-		return null;
+		return ClienteResponse.builder().idCliente(cliente.getIdCliente()).build();
 	}
 
 }
