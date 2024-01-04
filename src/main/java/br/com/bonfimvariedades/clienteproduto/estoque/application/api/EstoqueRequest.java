26303b08eafdd88af7d40a00bb74a4719c8b613b
matheusprata <matheusprata.ti@gmail.com>
2024-01-03 23:50:22 -0300

    retirada do extends e equals para implementação somente do produto

--- src/main/java/br/com/bonfimvariedades/clienteproduto/estoque/application/api/EstoqueRequest.java
+++ src/main/java/br/com/bonfimvariedades/clienteproduto/estoque/application/api/EstoqueRequest.java
@@ -1,18 +1,15 @@
 package br.com.bonfimvariedades.clienteproduto.estoque.application.api;
 
-import br.com.bonfimvariedades.clienteproduto.pedido.application.api.request.SolicitacaoRequest;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@EqualsAndHashCode(callSuper = true)
 @Value
-public class EstoqueRequest extends SolicitacaoRequest {
+public class EstoqueRequest{
     @NotNull(message = "Necessario o id do produto")
     UUID idProduto;
     @NotNull
